56f41138d9c94ff413bb9a4bcf045e243bef3b38
ZE-BOSS <ikchristo19@gmail.com>
2025-04-14 15:55:56 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Ingredients from './components/Ingredients';
