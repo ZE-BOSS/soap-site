1654e4bd2cb379a94a74e4e3e7ad0680d9e9761a
ZE-BOSS <ikchristo19@gmail.com>
2025-04-17 16:34:47 +0100

    Wenwaw update

--- src/components/Ingredients.tsx
+++ src/components/Ingredients.tsx
@@ -13,38 +13,38 @@ const Ingredients = () => {
       <h2 className="text-2xl font-bold mb-8">Why Choose Wenwaw?</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-6">
         <Card className='flex justify-center items-center bg-blue-50'> 
-          <CardContent className="flex p-2 md:p-4 gap-1 md:gap-4">
+          <CardContent className="flex p-2 md:p-4 justify-center items-center gap-1 md:gap-4">
             <Leaf size={90} color='lightgreen' />
             <div className='text-left'>
               <h3 className="font-semibold">Powerful Cleaning</h3>
-              <p className="text-sm">Cuts grease, grime, and dirt easily</p>
+              <p className="text-xs">Cuts grease, grime, and dirt easily</p>
             </div>
           </CardContent>
         </Card>
         <Card className='flex justify-center items-center bg-blue-50'> 
-          <CardContent className="flex p-2 md:p-4 gap-2 md:gap-4">
+          <CardContent className="flex p-2 justify-center items-center md:p-4 gap-2 md:gap-4">
             <Feather size={70} color='lightgreen' />
             <div className='text-left'>
               <h3 className="font-semibold">Multipurpose Use</h3>
-              <p className="text-sm">Cleans kitchen, bathroom, laundry, car</p>
+              <p className="text-xs">Cleans kitchen, bathroom, laundry</p>
             </div>
           </CardContent>
         </Card>
         <Card className='flex justify-center items-center bg-blue-50'> 
-          <CardContent className="flex p-2 md:p-4 gap-2 md:gap-4">
+          <CardContent className="flex p-2 justify-center items-center md:p-4 gap-2 md:gap-4">
             <Salad size={80} color='lightgreen' />
             <div className='text-left'>
               <h3 className="font-semibold">Long-Lasting Formula</h3>
-              <p className="text-sm">One drop gives more washes</p>
+              <p className="text-xs">One drop gives more washes</p>
             </div>
           </CardContent>
         </Card>
         <Card className='flex justify-center items-center bg-orange-50'> 
-          <CardContent className="flex p-2 md:p-4 gap-2 md:gap-4">
+          <CardContent className="flex p-2 justify-center items-center md:p-4 gap-2 md:gap-4">
             <Apple size={70} color='orange' />
             <div className='text-left'>
               <h3 className="font-semibold">Eco-Friendly Ingredients</h3>
-              <p className="text-sm">Gentle, effective, and environmentally safe</p>
+              <p className="text-xs">Gentle, effective, and environmentally safe</p>
             </div>
           </CardContent>
         </Card>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,10 @@ const Navbar = () => {
                 alt={`Wenwaw Logo`}
                 className="h-10 transition-all w-10 duration-700 ease-in-out object-fill"
             />
-            <p className='text-2xl md:text-4xl text-blue-900 font-bold'>wenwaw</p>
+            <div>
+              <p className='text-2xl md:text-4xl text-blue-900 font-bold'>wenwaw</p>
+              <p className='text-xs md:text-lg text-gray-900 font-thin'>One drop, pure magic</p>
+            </div>
         </div>
       </nav>
     );
